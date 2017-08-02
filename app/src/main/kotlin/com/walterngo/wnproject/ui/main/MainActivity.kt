4c2e5d1e1bbc6fce8626343003976ffd3617729c
Walter Ngo <walter.ngo@outware.com.au>
2017-08-02 13:39:31 +1000

    Optimise imports

--- app/src/main/kotlin/com/walterngo/wnproject/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/walterngo/wnproject/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.walterngo.wnproject.ui.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.walterngo.wnproject.R
 
 class MainActivity : AppCompatActivity() {
